d4d94f068363910ec4130135ff0847b46c256ff9
Abigail Marticio <marticiobgl@gmail.com>
2021-03-14 22:25:14 +0800

    chore: add sample on string formatting

---
+++ more_about_str/sample_3.py
@@ -0,0 +1,8 @@
+# String Formatting
+
+name = 'Alice'
+place = 'Main Street'
+time = '6 pm'
+
+print('Hello ' + name + ', you are invited to a party at ' + place + ' at ' + time )
+print('Hello %s, you are invited to a party at %s at %s' % (name, place, time))
