bc4cef4ee93ca4cb5b6f5940cb7094cf6c560545
Abigail Marticio <marticiobgl@gmail.com>
2021-03-16 21:37:12 +0800

    chore: add sample on dollar char

--- regular_expressions/sample_5.py
+++ regular_expressions/sample_5.py
@@ -13,3 +13,15 @@ begins_with_hello_Regex = re.compile(r'^Hello')
 match_object = begins_with_hello_Regex.search(message)
 print(match_object) 
 # None
+
+message = 'Hello world!'
+ends_with_world_Regex = re.compile(r'world!$')
+match_object = ends_with_world_Regex.search(message)
+print(match_object) 
+# <re.Match object; span=(6, 12), match='world!'>
+
+message = 'Hello world!asdfghjkl'
+ends_with_world_Regex = re.compile(r'world!$')
+match_object = ends_with_world_Regex.search(message)
+print(match_object) 
+# None
