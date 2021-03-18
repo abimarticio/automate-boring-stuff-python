81ce87af20e67c3ad2785c416292bf5557df78d3
Abigail Marticio <marticiobgl@gmail.com>
2021-03-18 23:03:09 +0800

    chore: add comments

--- regular_expressions/sample_6.py
+++ regular_expressions/sample_6.py
@@ -1,7 +1,9 @@
 # Regex sub() and Vebrose Mode
-
 import re
 
+
+# The sub() regex method will substitute matches with some other text.
+# Using \1, \2 and so will substitute group 1, 2, etc in the regex pattern.
 message = 'Agent Alice gave the secret documents to Agent Bob.'
 names_regex = re.compile(r'Agent \w+')
 match_object = names_regex.findall(message)
